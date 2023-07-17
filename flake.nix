0670bbb4cd336dbe738bf00e3c687b293522c5d0
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-07-18 02:25:46 +0800

    :construction_worker: Update CD

--- flake.nix
+++ flake.nix
@@ -11,8 +11,7 @@
         {
           formatter = nixpkgs-fmt;
           packages.default = buildPythonApplication rec {
-            pname = "pkgbuild-language-server";
-            version = "";
+            name = "pkgbuild-language-server";
             src = self;
             format = "pyproject";
             disabled = pythonOlder "3.6";
